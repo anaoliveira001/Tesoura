4d32b9a8ce264b5569c7573968253c612c0f9b13
anaoliveira001 <anaestefanodeoliveira@outlook.com>
2024-10-28 14:24:19 +0000

    Add project files.

---
+++ Tesoura/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesoura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tesoura tesoura = new Tesoura("Rosa");
+
+            tesoura.Cortar("plastico");
+            tesoura.Cortar("metal");
+            tesoura.Cortar("ferro");
+
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tesoura/Tesoura.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesoura
+{
+    internal class Tesoura
+    {
+        string cor;
+        int totalUso;
+
+        // método não usa-se void
+        public Tesoura(string cor)
+        {
+            this.cor = cor;
+            this.totalUso = 100;
+
+        }
+        public void Cortar(string material)
+        {
+            material = material.ToLower();
+
+            if (this.totalUso <= 0)
+            {
+                Console.WriteLine("A tesoura quebrou");
+                return;
+            }
+
+            if (material == "papel")
+                this.totalUso--;
+            else if (material == "metal")
+                this.totalUso -= 20;
+            else if (material == "plastico")
+                this.totalUso -= 70;
+            else
+                Console.WriteLine("Não pode cortar este Material com esta tesoura, use outro tipo");
+
+            this.UsosRestantes();
+        }
+
+        public void UsosRestantes()
+        {
+            Console.WriteLine($"A tesoura tem o total de: {this.totalUso} usos");
+        }
+    }
+}
